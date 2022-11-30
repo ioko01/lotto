3f87e05cb55663998d53a27a299d38c7903708da
unknown <ioko.peezaza@gmail.com>
2022-11-30 23:21:30 +0700

    commit

--- public/js/digits.js
+++ public/js/digits.js
@@ -1,4 +1,5 @@
 
+let temp_digits = [];
 function filter_numbers(e) {
     const regex = /[\D\sa-zA-Zก-ฮ]/;
     const numbers = e.value.split(regex);
@@ -11,6 +12,7 @@ function filter_numbers(e) {
             show_digits.append(`
             <button onclick="remove_numbers(this)">${res}</button>
             `)
+            temp_digits.push(res);
         }
     });
 }
@@ -20,27 +22,76 @@ function remove_numbers(e) {
 }
 
 function add_orders(e) {
-    const top = $("#top_digits");
-    const bottom = $("#bottom_digits");
+    const top_digits = $("#top_digits");
+    const bottom_digits = $("#bottom_digits");
     const show_digits = $("#show_digits");
-    const orders = $(".card-footer");
+    const orders = $("#footer_digits");
     if (!show_digits.html()) {
         console.log("ใส่เลขที่จะเล่น");
     } else {
-        if (!top.val() && !bottom.val()) {
+        if (!top_digits.val() && !bottom_digits.val()) {
             console.log("ใส่ราคา");
         } else {
+
             orders.append(`
-            <div class="d-flex justify-content-between">
-                <div class="detail w-25">
-                ${$("button.active").html()} <br/>บน x ล่าง<br/>${top.val() ? top.val() : 0} x ${bottom.val() ? bottom.val() : 0}
+            <div class="d-flex justify-content-between p-2 temp_orders">
+                <div class="d-flex flex-column text-center">
+                    <div class="detail1">
+                    ${$("button.active").html()}
+                    </div>
+                    <div class="detail2">
+                    บน x ล่าง
+                    </div>
+                    <div class="detail3">
+                    ${top_digits.val() ? top_digits.val() : 0} x ${bottom_digits.val() ? bottom_digits.val() : 0}
+                    </div>
                 </div>
-                <div class="orders w-50">22 01</div>
-            <div class="remove">
-                <button>remove</button>
-            </div>
-        </div>`)
+                <div class="orders w-50">${temp_digits.join().replaceAll(",", " ")}</div>
+                <div class="remove">
+                    <button onclick="remove_orders(this)">remove</button>
+                </div>
+            </div>`);
+
+
+            // console.log(digits);
+            show_digits.html(``);
+            top_digits.val(``);
+            bottom_digits.val(``);
+            temp_digits = [];
+
+
         }
     }
 
+}
+
+function remove_orders(e) {
+    $(e).parent().parent().remove();
+
+}
+
+function confirm_orders() {
+    const orders_price = $(".detail3");
+    const orders_bill = $(".orders");
+    
+    const digits = [];
+    for (let i = 0; i < orders_price.length; i++) {
+        const price = orders_price[i].innerHTML.trim().replaceAll(" ", "").split("x");
+        const orders = orders_bill[i].innerHTML.split(" ");
+
+        const bills = orders.map(order => {
+            const bill = `|NF|${order}|PTF|${price[0]}|PBF|${price[1]}`;
+            digits.push(bill);
+            return bill;
+        });
+    }
+
+    digits.map(digit => {
+        split_digits = digit.split(/[|NF||PTF||PBF|]/);
+        const filter_digits = split_digits.filter(res => {
+            return res !== null && res !== false && res !== "";
+        });
+        console.log(filter_digits[0]);
+    })
+    // console.log(digits);
 }
--- resources/views/frontend/pages/episode.blade.php
+++ resources/views/frontend/pages/episode.blade.php
@@ -6,20 +6,28 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card border border-warning">
-                    <div class="card-header">
-                        <button class="btn btn-outline-warning active">2 ตัว</button>
-                        <button class="btn btn-outline-warning">3 ตัว</button>
-                        <div id="show_digits"></div>
+                    <div class="card-body">
+                        <div style="gap: 10px;white-space: nowrap;" id="head_digits" class="d-flex">
+                            <button class="btn btn-outline-warning active">2 ตัว</button>
+                            <button class="btn btn-outline-warning">3 ตัว</button>
+                        </div>
+                        <div id="show_digits" class="py-3"></div>
+                        <div id="body_digits" style="gap: 10px;white-space: nowrap;" class="d-flex">
+                            <input onkeyup="filter_numbers(this)" id="input_digits" class="form-control" type="text"
+                                placeholder="ใส่เลข">
+                            <button type="button" class="btn btn-success">กลับเลข</button>
+                            <input id="top_digits" class="form-control" type="number" placeholder="บน">
+                            <input id="bottom_digits" class="form-control" type="number" placeholder="ล่าง">
+                            <button onclick="add_orders(this)" type="button" class="btn btn-primary">เพิ่มบิล</button>
+                        </div>
+                        <div id="footer_digits">
+
+                        </div>
+                        <div id="submit_digits" class="py-3">
+                            <button type="button" class="btn btn-success" onclick="confirm_orders()">ยืนยัน</button>
+                        </div>
+
                     </div>
-                    <div style="gap: 10px;white-space: nowrap;" class="card-body d-flex">
-                        <input onkeyup="filter_numbers(this)" id="input_digits" class="form-control" type="text"
-                            placeholder="ใส่เลข">
-                        <button type="button" class="btn btn-success">กลับเลข</button>
-                        <input id="top_digits" class="form-control" type="number" placeholder="บน">
-                        <input id="bottom_digits" class="form-control" type="number" placeholder="ล่าง">
-                        <button onclick="add_orders(this)" type="button" class="btn btn-primary">เพิ่มบิล</button>
-                    </div>
-                    <div class="card-footer"></div>
                 </div>
             </div>
         </div>
